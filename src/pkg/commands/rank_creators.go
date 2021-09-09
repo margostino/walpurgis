e2390de84c8f1aae165750977484e6ff63f16c7e
Martin D'Agostino <martin.dagostino@klarna.com>
2021-09-09 22:25:11 +0200

    clean up rank

--- src/pkg/commands/rank_creators.go
+++ src/pkg/commands/rank_creators.go
@@ -5,46 +5,74 @@ import (
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/margostino/walpurgis/pkg/client"
 	"github.com/margostino/walpurgis/pkg/db"
+	"github.com/margostino/walpurgis/pkg/helper"
 	"sort"
 	"strconv"
+	"sync"
 	"time"
 )
 
 // TODO: call in parallel, improve performance
 
 func RankCreators() {
-	var profiles = make([]db.UserInfo, 0)
 	users := db.LoadUsersData()
+
 	ch := make(chan db.UserInfo)
-	for _, user := range users {
-		//wg.Add(1)
-		go func(u db.User) {
-			var lastActivity time.Time
-			id, _ := strconv.ParseInt(u.ID, 10, 64)
-			tweets, _, _ := client.Twitter.Timelines.UserTimeline(&twitter.UserTimelineParams{
-				UserID: id,
-				Count:  1,
-			})
+	wg := sync.WaitGroup{}
+	wg.Add(len(users))
+	wgAppender := sync.WaitGroup{}
+	wgAppender.Add(len(users))
 
-			if len(tweets) > 0 {
-				lastActivity, _ = tweets[0].CreatedAtTime()
-			}
-			userInfo := db.UserInfo{Username: u.Username, LastActivity: lastActivity}
-			ch <- userInfo
-		}(user)
+	getLastGetLastActivityOf(&users, ch, &wg)
+
+	wg.Wait()
+	profiles := getUserProfile(users, ch, &wgAppender)
+	wgAppender.Wait()
+	sortByLastActivity(&profiles)
+	print(&profiles)
+	close(ch)
+}
+
+func getLastGetLastActivityOf(users *[]db.User, ch chan<- db.UserInfo, wg *sync.WaitGroup) {
+	for _, user := range *users {
+		go getLastActivityBy(user, ch)
+		wg.Done()
 	}
+}
 
+func getLastActivityBy(user db.User, ch chan<- db.UserInfo) {
+	var lastActivity time.Time
+	id, _ := strconv.ParseInt(user.ID, 10, 64)
+	tweets, _, err := client.Twitter.Timelines.UserTimeline(&twitter.UserTimelineParams{
+		UserID: id,
+		Count:  1,
+	})
+	helper.Check(err)
+	if len(tweets) > 0 {
+		lastActivity, _ = tweets[0].CreatedAtTime()
+	}
+	userInfo := db.UserInfo{Username: user.Username, LastActivity: lastActivity}
+	ch <- userInfo
+}
+
+func getUserProfile(users []db.User, ch <-chan db.UserInfo, wg *sync.WaitGroup) []db.UserInfo {
+	var profiles = make([]db.UserInfo, 0)
 	for i := 0; i < len(users); i++ {
 		user := <-ch
 		profiles = append(profiles, db.UserInfo{Username: user.Username, LastActivity: user.LastActivity})
+		wg.Done()
 	}
+	return profiles
+}
 
-	sort.SliceStable(profiles, func(i, j int) bool {
-		return profiles[i].LastActivity.Before(profiles[j].LastActivity)
+func sortByLastActivity(profiles *[]db.UserInfo) {
+	sort.SliceStable(*profiles, func(i, j int) bool {
+		return (*profiles)[i].LastActivity.Before((*profiles)[j].LastActivity)
 	})
+}
 
-	for _, profile := range profiles {
+func print(profiles *[]db.UserInfo) {
+	for _, profile := range *profiles {
 		fmt.Printf("[%s] - %s]\n", profile.Username, profile.LastActivity)
 	}
-
 }
