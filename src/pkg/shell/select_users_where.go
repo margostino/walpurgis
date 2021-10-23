7267225cbff927e5646851398b8782ded72ceeef
Martin D'Agostino <martin.dagostino@klarna.com>
2021-10-23 19:00:34 +0200

    impl select users by condition

--- src/pkg/shell/select_users_where.go
+++ src/pkg/shell/select_users_where.go
@@ -10,8 +10,34 @@ func ExecuteSelectUsersWhere(args []string) {
 	users := db.LoadUsersData()
 
 	for _, user := range users {
-		if strings.Contains(user.Description, args[0]) {
+		match := false
+		key := getKey(args[0], user)
+		switch strings.ToLower(args[1]) {
+		case "like":
+			match = strings.Contains(key, args[2])
+		case "=":
+			match = key == args[2]
+		case "not like":
+			match = !strings.Contains(key, args[2])
+		}
+
+		if match {
 			fmt.Printf("[%s] - %s]\n", user.Username, user.Description)
 		}
+	}
+}
+
+func getKey(param string, user *db.User) string {
+	switch strings.ToLower(param) {
+	case "description":
+		return user.Description
+	case "email":
+		return user.Email
+	case "status":
+		return user.StatusText
+	case "name":
+		return user.Name
+	default:
+		return ""
 	}
 }
